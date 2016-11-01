9d45f5743faa1f037c586516a0cc818847e8359a
Grant Tuttle <grant.tuttle@gmail.com>
2016-10-31 21:05:43 -0400

    beginnings of passing the url to the program.

--- main.py
+++ main.py
@@ -1,38 +1,54 @@
-#!/usr/local/bin/python3
+#! /usr/local/bin/python3
 import urllib.request
 import os
 import time
 from bs4 import BeautifulSoup
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("url", help="pass the URL that you would like to analyze")
+args = parser.parse_args()
 
 filename = "hrefs"
 visited_filename = "visited_hrefs"
-dir_name = "../scraped_files/"+time.strftime("%Y_%m_%d_%H_%M_%S",time.gmtime())
+rejected_filename = "rejected_hrefs"
+
+dir_name = "./.%%"+time.strftime("%m_%d_%H_%M_%S",time.gmtime())
 os.makedirs(dir_name)
 
 THE_FILE = open(dir_name+"/"+filename,'w')
 THE_VISITED_FILE = open(dir_name+"/"+visited_filename,'w')
-THE_URL = 'https://freshharvest.deliverybizpro.com/home.php#'
+THE_REJECTED_FILE = open(dir_name+"/"+rejected_filename,'w')
+
+THE_URL = args.url
 
 page = urllib.request.urlopen(THE_URL).read()
 soup =  BeautifulSoup(page,"html.parser")
 urls = []
 
 for a in soup.find_all('a'):
+    default_url = THE_URL
     href = a.get('href')
 
     if href[0:10] == "javascript":
+        THE_REJECTED_FILE.write(href + "\n")
         continue
     elif href[0]=="/" or href[0]=="#":
-        path = "#" if href=="/home.php" else href
-        href = THE_URL + path
-    elif href[0].isalpha() and href[0:6]!="mailto" and href[0:4]!="http":
-        href = THE_URL + "/" + href
+        href = "#" if href=="/home.php" else href
+    elif href[0:6]=="mailto":
+        THE_REJECTED_FILE.write(href + "\n")
+        continue
+    elif href[0:4]=="http":
+        default_url = ""
+    elif href[0].isalpha():
+        href = "/" + href
 
     if href in urls:
         continue
     else:
         urls.append(href)
-        THE_FILE.write(href + "\n")
+        THE_FILE.write(default_url + href + "\n")
+
 THE_FILE.close()
 THE_VISITED_FILE.write(THE_URL + "\n")
 THE_VISITED_FILE.close()
